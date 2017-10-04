d56c28190c69124f56c28ac04746811149ea31b6
Derek Stotz <derek@springboardretail.com>
2017-10-04 11:35:04 -0500

    Added support for customer resource

---
+++ lib/lightspeed/customer.rb
@@ -0,0 +1,32 @@
+require_relative 'resource'
+
+module Lightspeed
+  class Customer < Lightspeed::Resource
+    alias_method :archive, :destroy
+
+    fields(
+      customerID: :id,
+      firstName: :string,
+      lastName: :string,
+      dob: :datetime,
+      archived: :boolean,
+      title: :string,
+      company: :string,
+      companyRegistrationNumber: :string,
+      vatNumber: :string,
+      createTime: :datetime,
+      timeStamp: :datetime,
+      creditAccountID: :id,
+      customerTypeID: :id,
+      discountID: :id,
+      taxCategoryID: :id,
+      Contact: :hash,
+      CreditAccount: :hash,
+      CustomerType: :hash,
+      Discount: :hash,
+      Note: :hash,
+      TaxCategory: :hash,
+      CustomFieldValues: :hash,
+    )
+  end
+end
